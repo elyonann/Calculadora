a57b03c09e208e4756a5e9772d72eab8d7a32fb8
elyonann <elyonann@gmail.com>
2023-03-25 11:09:03 -0300

    first commit

---
+++ Calculadora.py
@@ -0,0 +1,8 @@
+def calculadora():
+    # Obtenha os dois números e a operção  a ser realizada pelo usuário
+
+    num1 = float(input("Digite o primeiro número: "))
+    num2 = float(input("Digite o segundo número: "))
+    op = input("Digite a operação desejada (+, -, *, /): ")
+
+    
